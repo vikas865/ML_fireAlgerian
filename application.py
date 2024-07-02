d3cab852bc27b81fc4a19d53529957e432fe0e38
vikas865 <vikas865@gmail.com>
2024-07-02 14:58:57 +0530

    updated for beanstalk

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-app=Flask(__name__)
+application=Flask(__name__)
+app=application
 
 
 model=pickle.load(open('models/ridge_lr.pkl','rb'))
